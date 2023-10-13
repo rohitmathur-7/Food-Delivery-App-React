c422284d4a38ee5a3bce67dc9df34d0a46e75cbf
rohitmathur-7 <rohitmathur.mathur.6@gmail.com>
2023-10-13 08:50:17 +0530

    Namaste React ep 1

---
+++ app.js
@@ -0,0 +1,14 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "heading1" }, "Hi from H1 element"),
+    React.createElement("h2", { id: "heading2" }, "Hi from H2 element"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading3" }, "Hi from H1 element"),
+    React.createElement("h2", { id: "heading4" }, "Hi from H2 element"),
+  ]),
+]);
+
+root.render(parent);
