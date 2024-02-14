3bb1a14974d383b99d657d80a19bf88af86439b8
rohitmathur-7 <rohitmathur.mathur.6@gmail.com>
2024-02-15 00:42:52 +0530

    Updated Swiggy's API

--- src/components/Body.js
+++ src/components/Body.js
@@ -12,21 +12,23 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.9243316&lng=75.8123829&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
 
     const json = await data.json();
-    console.log(json);
+    console.log("🚀 ~ fetchData ~ json:", json);
+    console.log(
+      "🚀 ~ fetchData ~ json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants:",
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
     setListOfRes(
-      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
   };
 
-  if (listOfRes.length === 0) {
-    return <Shimmer />;
-  }
-
-  return (
+  return listOfRes.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="app-body">
       <input
         type="text"
